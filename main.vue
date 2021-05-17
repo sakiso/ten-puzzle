18d13e6eba7e495f4d56084876c996719f1252af
sakiso <sakis4ssy@gmail.com>
2021-05-18 00:22:55 +0900

    first commit

---
+++ main.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <v-app-bar v-if="!$vuetify.breakpoint.xsOnly" app height="50">
+      <tabBar></tabBar>
+    </v-app-bar>
+
+    <router-view />
+
+    <v-bottom-navigation v-if="$vuetify.breakpoint.xsOnly" app height="50">
+      <tabBar></tabBar>
+    </v-bottom-navigation>
+  </div>
+</template>
+
+<script>
+import TabBar from '@/components/tabBar.vue'
+
+export default {
+  name: 'Main',
+  components: {
+    TabBar,
+  },
+  data: function () {
+    return {
+      drawer: false,
+    }
+  },
+}
+</script>
+
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,123 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-container>
+      <v-row>
+        <v-col>
+          <div>
+            <v-btn
+              @click="selectNumber(number1)"
+              :disabled="selectCompleteFlg"
+              >{{ number1 }}</v-btn
+            >
+            <v-btn
+              @click="selectNumber(number2)"
+              :disabled="selectCompleteFlg"
+              >{{ number2 }}</v-btn
+            >
+            <v-btn
+              @click="selectNumber(number3)"
+              :disabled="selectCompleteFlg"
+              >{{ number3 }}</v-btn
+            >
+            <v-btn
+              @click="selectNumber(number4)"
+              :disabled="selectCompleteFlg"
+              >{{ number4 }}</v-btn
+            >
+          </div>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <div>
+            <v-btn>+</v-btn>
+            <v-btn>-</v-btn>
+            <v-btn>×</v-btn>
+            <v-btn>÷</v-btn>
+          </div>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <div>
+            {{ selectedNumber }}
+          </div>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <div>
+            <v-btn
+              @click="
+                setNumbers();
+                deleteSelectedNumbers();
+              "
+              >reload</v-btn
+            >
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+
+  components: {},
+  data: function () {
+    return {
+      number1: 0,
+      number2: 0,
+      number3: 0,
+      number4: 0,
+      selectedNumber: [],
+      selectNumberCounter: 0,
+      selectCompleteFlg: false,
+    };
+  },
+
+  created: function () {
+    //4つの異なる乱数(1~9までの整数）を生成する
+    this.setNumbers();
+  },
+
+  methods: {
+    deleteSelectedNumbers() {
+      this.selectedNumber = [];
+      this.selectCompleteFlg = false;
+    },
+    setNumbers() {
+      //4つの異なる乱数(1~9までの整数）を生成する
+      //1つ目
+      this.number1 = Math.floor(Math.random() * 9) + 1;
+      //2つ目
+      do {
+        this.number2 = Math.floor(Math.random() * 9) + 1;
+      } while (this.number2 === this.number1);
+      //3つ目
+      do {
+        this.number3 = Math.floor(Math.random() * 9) + 1;
+      } while (this.number3 === this.number2 || this.number3 === this.number1);
+      //4つ目
+      do {
+        this.number4 = Math.floor(Math.random() * 9) + 1;
+      } while (
+        this.number4 === this.number3 ||
+        this.number4 === this.number2 ||
+        this.number4 === this.number1
+      );
+    },
+
+    selectNumber(num) {
+      this.selectedNumber.push(num);
+      //4つ選んだらselectCompleteFlgを立てて完了扱いにする
+      this.selectNumberCounter += 1;
+      if (this.selectNumberCounter === 4) {
+        this.selectCompleteFlg = true;
+      }
+    },
+  },
+};
 </script>
